8f670b05eb85b2c8de30bce7c42d72c87436cf42
Gustavo Sarturi <34655704+gustavosarturi@users.noreply.github.com>
2018-11-01 09:35:24 -0300

    Versão BETA

--- main.jl
+++ main.jl
@@ -28,14 +28,37 @@ function regressao_polinomial(x, y, p)
     return β
 end
 
-function kfold(x, y; num_folds = 5, max_p=15)
+function kfold(x::Array, y::Array; num_folds = 5, max_p = 15)
+    if length(x) != length(y)
+        error("Dados são incompatíveis!")
+    end
+
     m = length(x)
-    k = div(m, num_folds)
     I = randperm(m)
+    fold_size = div(m, num_folds)
+    E_Treino(num_folds, max_p)
+    E_teste(num_folds, max_p)
 
-    # Seu código aqui
+    for fold=1:m
+        cjto_teste = fold
+        cjto_treino = setdiff(1:m, fold)
+        Testex = X[cjto_teste]
+        Testey = Y[cjto_teste]
+        Treinox = Y[cjto_treino]
+        Treinoy = Y[cjto_treino]
+        for p = 1:max_p
+            β = regressao_polinomial(Treinox, Treinoy, p)
+            xlin = range(0, stop=2pi, length=100)
 
-    png("kfold")
+            y_pred = [β[1] + sum(β[j + 1] * xi^j for j = 1:p) for xi = xlin]
+
+            E_teste = 1/2
+            scatter(X, Y, leg=false)
+            ylims!(4.9, 8.1)
+        end
+    end
+    
+    png("test_$p")
 end
 
 main()
