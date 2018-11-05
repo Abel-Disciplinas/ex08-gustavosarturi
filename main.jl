cb06b82346c2dedf6f870058e258fab334ac1611
Gustavo Sarturi <34655704+gustavosarturi@users.noreply.github.com>
2018-11-05 15:55:36 -0200

    Versão Brasileira: Hebert Richard

--- main.jl
+++ main.jl
@@ -1,10 +1,3 @@
-using Plots
-gr(size=(600, 400))
-default(fmt = :png)
-
-data = readcsv("dados.csv")
-x, y = data[:,1], data[:,2]
-
 function regressao_polinomial(x::Array, y::Array, p::Integer)
     m = length(x)
     A = ones(m, p+1)
@@ -16,18 +9,6 @@ function regressao_polinomial(x::Array, y::Array, p::Integer)
     β = (A' * A) \ (A' * y)
     return β
 end
-
-function regressao_polinomial(x, y, p)
-    m = length(x)
-    A = [ones(m) [x[i]^j for i = 1:m, j = 1:p]]
-    β = (A' * A) \ (A' * y)
-    return β
-end
-
-
-#Vamos supor que nossos dados sejam esses:
-x = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-y = [11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
 
 function kfold(x, y; num_folds = 5, max_p=15)
     num_folds = 2
@@ -43,7 +24,6 @@ function kfold(x, y; num_folds = 5, max_p=15)
     i_fold = 1
 
     for fold=1:fold_size:m
-        #println("i_fold é = ", i_fold)
         cjto_teste = fold:fold + fold_size - 1
         cjto_treino = setdiff(1:m, cjto_teste)
 
@@ -58,22 +38,28 @@ function kfold(x, y; num_folds = 5, max_p=15)
             E_teste[i_fold,p] = norm(y[cjto_teste] - y_pred[cjto_teste])^2
             E_treino[i_fold,p] = norm(y[cjto_treino] - y_pred[cjto_treino])^2
         end
-        println("E_teste = ", E_teste)
-        println("E_treino = ", E_treino)
-        png("kfold-$fold")
         i_fold += 1
     end
 end
-println()
 
-kfold(x,y)
+function main()
 
-E_teste
+    data = readcsv("dados.csv")
+    x, y = data[:,1], data[:,2]
 
-#=for fold=1:fold_size:m
-    cjto_teste = fold
-    cjto_treino = setdiff(1:m, fold)
-    for k=fold:fold + fold_size - 1 #Separa os valores de treino
-        E_teste = [x[k], y[k]]
+    kfold(x, y)
+    p = 15
+    xlin = linspace(extrema(x)..., 100)
+    β = regressao_polinomial(x, y, p)
+    ylin = β[1] * ones(100)
+    for j = 1:p
+        ylin .+= β[j+1] * xlin.^j
     end
-end=#
+    scatter(x, y, ms=3, c=:blue)
+    plot!(xlin, ylin, c=:red, lw=2)
+    png("ajuste_")
+    # Calcule a medida R²
+end
+
+main()
+
